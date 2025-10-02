5e58f5171c9892fb4994806455d84a8ece69a839
Roman Andreev <andreev.grizzly@gmail.com>
2025-10-03 02:09:58 +0300

    created app

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,34 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
 
 app = FastAPI()
-
+tasks = []
+task_id_counter = 1
+# выводим список всех полученных задач
 @app.get("/tasks")
 def get_tasks():
-    pass
-
+    return tasks
+# добавляем новую задачу, добавляем ее в список задач и увеличиваем счетчик id
 @app.post("/tasks")
-def create_task(task):
-    pass
-
+def create_task(name: str, condition: str = 'new'):
+    task = {'id': task_id_counter, 'name': name, 'condition': condition}
+    tasks.append(task)
+    task_id_counter += 1
+    return task
+# обновляем задачу , в случае отсутствия задачи вызываем ошибку
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
-
+def update_task(task_id: int, name:str, condition: str):
+    for task in tasks:
+        if task['id'] == task_id:
+            task[name] = name
+            task[condition] = condition
+        return task
+    raise HTTPException(status_code = 404, detail = 'task not found')
+# ищем задачу по id, если находи - удаляем, если нет - вызываем ошибку
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    for task in tasks:
+        if task['id'] == task_id:
+            tasks.remove(task)
+            return 'task deleted'
+    raise HTTPException(status_code = 404, detail = 'task not found')

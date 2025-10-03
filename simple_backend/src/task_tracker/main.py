4b673dfed62de7ed03ac6de1bcffa68fc188dbe5
Roman Andreev <andreev.grizzly@gmail.com>
2025-10-04 01:43:48 +0300

    comment main

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,39 +1,39 @@
-from fastapi import FastAPI
-from fastapi import HTTPException
-from storage import TaskStorage
-app = FastAPI()
-storage = TaskStorage()
+from fastapi import FastAPI                             # import FastApi class
+from fastapi import HTTPException                       # import HHTPExeption class
+from storage import TaskStorage                         # import TaskStorage class
+app = FastAPI()                                         # create copy of FastAPi class
+storage = TaskStorage()                                 # create copy of TaskStorage class
 # we display a list of all received tasks.
-@app.get("/tasks")
-def get_tasks():
-    return storage.load()
+@app.get("/tasks")                                      # use standart FastApi get decorator
+def get_tasks():                                        # get task method 
+    return storage.load()                               # return task lsit
 # We add a new task, add it to the task list, and increment the id counter.
-@app.post("/tasks")
-def create_task(name: str, condition: str = 'new'):
-    tasks = storage.load()
-    new_id = max((task['id'] for task in tasks), default = 0) + 1
-    task = {'id': new_id, 'name': name, 'condition': condition}
-    tasks.append(task)
-    storage.save(tasks)
-    return task
+@app.post("/tasks")                                     # use standart FastAPI post decorator
+def create_task(name: str, condition: str = 'new'):     # create new task method
+    tasks = storage.load()                              # task list now in tasks
+    new_id = max((task['id'] for task in tasks), default = 0) + 1    # crate new id fo task
+    task = {'id': new_id, 'name': name, 'condition': condition}  # task dict   
+    tasks.append(task)                                  # append task on list
+    storage.save(tasks)                                 # save task
+    return task                                         # return new task
 # We update the task; if the task is missing, we raise an error.
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int, name: str, condition: str):
-    tasks = storage.load()
-    for task in tasks:
-        if task['id'] == task_id:
-            task['name'] = name
-            task['condition'] = condition
-            storage.save(tasks)
-            return task
-    raise HTTPException(status_code = 404, detail = 'task not found')
+@app.put("/tasks/{task_id}")                            # use standart FastAPI put decorator
+def update_task(task_id: int, name: str, condition: str):  # update task meyhod
+    tasks = storage.load()                              # loads all tasks list
+    for task in tasks:                                  # finding task cicle
+        if task['id'] == task_id:                       # check task id in list
+            task['name'] = name                         # update name if id in list
+            task['condition'] = condition               # update status if id in list
+            storage.save(tasks)                         # save task in list
+            return task                                 # return updated task
+    raise HTTPException(status_code = 404, detail = 'task not found') # raise if id not in list
 # We search for a task by ID, if we find it, we delete it, if not, we raise an error
-@app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-    tasks = storage.load()
-    for task in tasks:
-        if task['id'] == task_id:
-            tasks.remove(task)
-            storage.save(tasks)
-            return 'task deleted'
-    raise HTTPException(status_code = 404, detail = 'task not found')
+@app.delete("/tasks/{task_id}")                         # use standart FastApi delete decorator
+def delete_task(task_id: int):                          # delete task method
+    tasks = storage.load()                              # loads all tasks list
+    for task in tasks:                                  # finding task cicle
+        if task['id'] == task_id:                       # check task id in list
+            tasks.remove(task)                          # remove task if id in list
+            storage.save(tasks)                         # save tasks list
+            return 'task deleted'                       # return delete message
+    raise HTTPException(status_code = 404, detail = 'task not found') # raise if id not in list

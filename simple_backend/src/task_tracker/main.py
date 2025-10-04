169e70f9fd82a61593b8b40357bb329f6378e223
Roman Andreev <andreev.grizzly@gmail.com>
2025-10-04 16:23:37 +0300

    task2 backend with state storage

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,20 +1,35 @@
 '''backend'''
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
-
+tasks = []
+task_id_counter = 1
+# return tasks list
 @app.get("/tasks")
 def get_tasks():
-    pass
-
+    return tasks
+#create a new task with id, name, and status 
 @app.post("/tasks")
-def create_task(task):
-    pass
-
+def create_task(name: str, condition: str = 'new'):
+    global task_id_counter
+    task = {'id': task_id_counter, 'name': name, 'condition': condition}
+    tasks.append(task)
+    task_id_counter += 1
+    return task
+#update task    
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
-
+def update_task(task_id: int, name: str, condition: str):
+    for task in tasks:
+        if task['id'] == task_id:
+            task['name'] = name
+            task['condition'] = condition
+            return task
+    raise HTTPException(status_code = 404, detail = 'task not found')
+#delete task
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    for task in tasks:
+        if task['id'] == task_id:
+            tasks.remove(task)
+            return 'task deleted'
+ 
